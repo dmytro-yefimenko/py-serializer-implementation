609b2a5b06fdbef2301c6ba920e722954aed59e3
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-05-20 17:45:08 +0300

    Fixed flake8

--- car/serializers.py
+++ car/serializers.py
@@ -6,9 +6,13 @@ class CarSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     manufacturer = serializers.CharField(max_length=64, required=True)
     model = serializers.CharField(max_length=64, required=True)
-    horse_powers = serializers.IntegerField(max_value=1914, min_value=1, required=True)
+    horse_powers = serializers.IntegerField(
+        max_value=1914, min_value=1, required=True
+    )
     is_broken = serializers.BooleanField()
-    problem_description = serializers.CharField(required=False, allow_blank=True)
+    problem_description = serializers.CharField(
+        required=False, allow_blank=True
+    )
 
     class Meta:
         model = Car
@@ -29,8 +33,12 @@ class CarSerializer(serializers.Serializer):
             "manufacturer", instance.manufacturer
         )
         instance.model = validated_data.get("model", instance.model)
-        instance.horse_powers = validated_data.get("horse_powers", instance.horse_powers)
-        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.horse_powers = validated_data.get(
+            "horse_powers", instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken", instance.is_broken
+        )
         instance.problem_description = validated_data.get(
             "problem_description", instance.problem_description
         )
